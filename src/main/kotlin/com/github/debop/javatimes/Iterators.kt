ea1c0d49e786238eb9596ec723663decf150da68
debop <@baekwon9>
2018-02-02 23:54:41 +0900

    Bump up kotlin-stdlib 1.2.21 add kotlinx-coroutines-rx 0.22.1

--- src/main/kotlin/com/github/debop/javatimes/Iterators.kt
+++ src/main/kotlin/com/github/debop/javatimes/Iterators.kt
@@ -8,7 +8,7 @@ import java.util.*
  */
 abstract class JavaDateIterator<T : Date> : Iterator<T> {
 
-  override final fun next(): T = nextJavaDate()
+  final override fun next(): T = nextJavaDate()
 
   /** Returns the next value in the sequence without boxing. */
   abstract fun nextJavaDate(): T
@@ -16,11 +16,11 @@ abstract class JavaDateIterator<T : Date> : Iterator<T> {
 }
 
 /**
- * An iterator over a sequence of values of type `java.util.Instant``.
+ * An iterator over a sequence of values of type `java.util.Instant`.
  */
 abstract class JavaInstantIterator : Iterator<Instant> {
 
-  override final fun next(): Instant = nextInstant()
+  final override fun next(): Instant = nextInstant()
 
   abstract fun nextInstant(): Instant
 
