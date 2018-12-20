a84b3e77d2726099dca0628c55d2389cedbe62b1
debop <debop@coupang.com>
2018-12-20 22:41:15 +0900

    Migrate build script from groovy to kotlin dsl

---
+++ java-time/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+tasks.withType<KotlinCompile> {
+    sourceCompatibility = "1.8"
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+
+    implementation("io.reactivex.rxjava2:rxjava:${extra.get("rxjava2")}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:${extra.get("coroutines")}")
+
+    implementation("io.github.microutils:kotlin-logging:1.6.22")
+    implementation("org.slf4j:slf4j-api:1.7.25")
+    testImplementation("ch.qos.logback:logback-classic:1.2.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${extra.get("junitJupiter")}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${extra.get("junitJupiter")}")
+}
