eec57bea6fb2986243e086ea929fb49a70cb9084
debop <debop@coupang.com>
2018-12-27 20:53:07 +0900

    Use JUnit 5 Parallel testing

--- java-time/src/test/kotlin/com/github/debop/javatimes/TemporalIntervalTest.kt
+++ java-time/src/test/kotlin/com/github/debop/javatimes/TemporalIntervalTest.kt
@@ -1,20 +1,29 @@
+/*
+ * Copyright (c) 2016. Sunghyouk Bae <sunghyouk.bae@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.debop.javatimes
 
 import mu.KLogging
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.time.OffsetDateTime
 import java.time.Period
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertNull
-import kotlin.test.assertTrue
 
-/**
- * TemporalIntervalTest
- *
- * @autor debop
- * @since 18. 4. 17
- */
 class TemporalIntervalTest : AbstractJavaTimesTest() {
 
     companion object : KLogging()
